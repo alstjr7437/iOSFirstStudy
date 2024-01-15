f5b59c037af691604f19528579c274f66d39f863
김민석|Minseok Kim <alstjr7437@naver.com>
2024-01-15 19:20:24 +0900

    ChatList 프로젝트 커밋

---
+++ ChatList/ChatList/ChatListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by KimMinSeok on 1/15/24.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    let chatList: [Chat] = Chat.list
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Data, Presentation, Layout
+        
+        // Data, Presentation
+        collectionView.dataSource = self
+        // Layout
+        collectionView.delegate = self
+    }
+}
+
+
+extension ChatListViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chatList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //gaurd 안넣으면 error
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let chat = chatList[indexPath.item]
+        cell.configure(chat)
+        
+        
+        return cell
+        
+    }
+    
+}
+
+extension ChatListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
+}
+
