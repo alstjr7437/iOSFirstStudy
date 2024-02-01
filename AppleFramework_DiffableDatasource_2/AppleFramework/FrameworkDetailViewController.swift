7b571c73b58505e0b11371fd693909a79982ba79
김민석|Minseok Kim <alstjr7437@naver.com>
2024-02-01 14:25:29 +0900

    애플 프레임 워크 모달 추가 작업

---
+++ AppleFramework_DiffableDatasource_2/AppleFramework/FrameworkDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FrameworkDetailViewController.swift
+//  AppleFramework
+//
+//  Created by KimMinSeok on 2/1/24.
+//
+
+import UIKit
+import SafariServices
+
+class FrameworkDetailViewController: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    var framework: AppleFramework = AppleFramework(name: "unknown", imageName: "", urlString: "", description: "")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        
+    }
+    
+    func updateUI(){
+        imgView.image = UIImage(named: framework.imageName)
+        titleLabel.text = framework.name
+        descriptionLabel.text = framework.description
+    }
+    
+
+    @IBAction func learnMoreTapped(_ sender: Any) {
+        
+        guard let url = URL(string: framework.urlString) else {
+            return
+        }
+        
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+    
+}
